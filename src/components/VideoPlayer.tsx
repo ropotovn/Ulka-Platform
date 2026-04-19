4deb43afe56c4c54057c0391acc845efe00fa5bf
ropotovn <165428964+ropotovn@users.noreply.github.com>
2026-04-19 16:56:12 +0300

    Update VideoPlayer.tsx

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -9,7 +9,7 @@ export function VideoPlayer({ title, subtitle }: VideoPlayerProps) {
   return (
     <div className="relative rounded-2xl overflow-hidden shadow-2xl bg-gray-900 aspect-video">
       <iframe
-        src="https://rutube.ru/play/embed/818504848d85e4826521d6cf2590182e/?skinColor=7fc6e3"
+        src="https://rutube.ru/video/private/818504848d85e4826521d6cf2590182e/?r=wd&p=Tpf4kBVAibq16v2pLKIpsQ"
         className="w-full h-full"
         style={{ border: "none" }}
         allow="clipboard-write; autoplay"
