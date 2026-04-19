ab5277061d7858b231d8397b89e7d917ea5bd1a4
ropotovn <165428964+ropotovn@users.noreply.github.com>
2026-04-19 16:57:35 +0300

    Update VideoPlayer.tsx

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -5,16 +5,15 @@ interface VideoPlayerProps {
   subtitle?: string;
 }
 
-export function VideoPlayer({ title, subtitle }: VideoPlayerProps) {
+export function VideoPlayer() {
   return (
     <div className="relative rounded-2xl overflow-hidden shadow-2xl bg-gray-900 aspect-video">
       <iframe
-        src="https://rutube.ru/video/private/818504848d85e4826521d6cf2590182e/?r=wd&p=Tpf4kBVAibq16v2pLKIpsQ"
+        src="https://rutube.ru/play/embed/818504848d85e4826521d6cf2590182e/?p=Tpf4kBVAibq16v2pLKIpsQ"
         className="w-full h-full"
         style={{ border: "none" }}
         allow="clipboard-write; autoplay"
         allowFullScreen
-        title={title || "ULKA Platform video"}
       />
     </div>
   );
