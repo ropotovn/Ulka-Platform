102d55605c3484c3dbc2c078f6ac01f475fb6884
ropotovn <165428964+ropotovn@users.noreply.github.com>
2026-05-14 20:23:07 +0300

    Update BookingModal.tsx

--- src/components/BookingModal.tsx
+++ src/components/BookingModal.tsx
@@ -50,7 +50,7 @@ const handleSubmit = async (e: React.FormEvent) => {
 
   // отправка в Google Sheets
   try {
-    await fetch("https://script.google.com/macros/s/AKfycbzX0g17JgHDWIn6tY0N0rvEJDkUMPmlfwdy7DeiaGIj7cq7pLEGA6OWNO3udhgDujOc/exe", {
+    await fetch("https://script.google.com/macros/s/AKfycbzX0g17JgHDWIn6tY0N0rvEJDkUMPmlfwdy7DeiaGIj7cq7pLEGA6OWNO3udhgDujOc/exec", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
