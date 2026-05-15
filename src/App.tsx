cdd62612e1a295958ce1e087ba5673c2525811b5
ropotovn <165428964+ropotovn@users.noreply.github.com>
2026-05-16 00:21:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ function App() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-white font-sans">
+    <div className="min-h-screen bg-white dark:bg-black font-sans dark:text-white transition-colors">
       <Navigation />
       <main>
         <Hero />
